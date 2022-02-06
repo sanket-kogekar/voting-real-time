84212fa25d5934603d19203eae2116584a49c081
sanket-kogekar <sanket@bhyve.io>
2022-02-06 10:06:57 +0530

    Changed URL

--- src/contexts/socket.js
+++ src/contexts/socket.js
@@ -4,7 +4,7 @@ import socketio from "socket.io-client";
 // const SOCKET_URL = "https://easy-voting.herokuapp.com/"
 //process.env.SOCKET_URL
 
-const SOCKET_URL = process.env.SOCKET_URL;
+const SOCKET_URL = "https://easy-voting.herokuapp.com";
 
 export const socket = socketio.connect(SOCKET_URL);
 export const SocketContext = createContext();
