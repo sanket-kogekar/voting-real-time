4f29f0d0ce8984a9a62fcb152be3d10aaaf26ca4
sanket-kogekar <kogekar@proton.me>
2023-01-30 22:06:52 +0530

    hosting on railway

--- src/contexts/socket.js
+++ src/contexts/socket.js
@@ -6,7 +6,8 @@ import socketio from "socket.io-client";
 
 // const SOCKET_URL = "http://localhost:5000";
 // const SOCKET_URL = "https://easy-voting.herokuapp.com";
-const SOCKET_URL = "https://voting-qh66.onrender.com/";
+// const SOCKET_URL = "https://voting-qh66.onrender.com/";
+const SOCKET_URL = "https://web-production-2ac3.up.railway.app/";
 
 export const socket = socketio.connect(SOCKET_URL);
 export const SocketContext = createContext();
