b31efd4f0d63fc3ea2b4cfe358eb9c9414f67127
sanket-kogekar <kogekar@proton.me>
2023-01-31 15:44:20 +0530

    easy fix

--- src/contexts/socket.js
+++ src/contexts/socket.js
@@ -7,7 +7,8 @@ import socketio from "socket.io-client";
 // const SOCKET_URL = "http://localhost:5000";
 // const SOCKET_URL = "https://easy-voting.herokuapp.com";
 // const SOCKET_URL = "https://voting-qh66.onrender.com/";
-const SOCKET_URL = "https://web-production-2ac3.up.railway.app/";
+// const SOCKET_URL = "https://web-production-2ac3.up.railway.app/";
+const SOCKET_URL = "https://easy-cyan-bighorn-sheep.cyclic.app/";
 
 export const socket = socketio.connect(SOCKET_URL);
 export const SocketContext = createContext();
